e0ff50eeeb7aa63035c3c712d5ad1abd2d75f8e1
Osman Polat <osmanplt21@gmail.com>
2024-08-16 15:25:37 +0300

    Menu app project defined

---
+++ 05-menu/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import items from "./data.js";
+import Categories from "./Categories.jsx";
+import Menu from "./Menu.jsx";
+
+const allCategories=
+['all', ...new Set(items.map((item)=>item.category))]
+
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems=(category)=>{
+    if(category==="all"){
+      setMenuItems(items)
+      return
+    }
+    const newItems=items.filter((item)=>item.category===category)
+    setMenuItems(newItems)
+  }
+
+  return (
+    <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems}/>
+    </section>
+  </main>
+  )
+}
+
+export default App;
