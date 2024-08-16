5ce5ffb8c6487631cf414b0379c7d0a3fdab25dd
Osman Polat <osmanplt21@gmail.com>
2024-08-16 11:12:53 +0300

    app.js updated

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import questionsData from "./data.js";
+import data from "./data.js";
 import React from "react";
 import Question from "./Questions.jsx";
 
  
 function App() {
 
-const [questions,setQuestions]=useState(questionsData)
+const [questions,setQuestions]=useState(data)
 
   return  <main>
     <div className="container">
       <h3>questions and answers about login</h3>
       <section className="info">
        {
-        questionsData.map((question)=>{
+        questions.map((question)=>{
           return <Question key={question.id}{...question} />
 })
        } 
